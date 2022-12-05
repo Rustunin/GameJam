4e16538c54b31649841861cba71b8395aa4c73a1
WebDevvvv <117771890+Rustunin@users.noreply.github.com>
2022-12-05 22:30:00 +0600

    da

---
+++ GameJam/Assets/Scripts/EnemyWatching.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWatching : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector3 dir;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = player.transform.position;
+
+        dir = targetPos - (Vector3)transform.position;
+
+        transform.forward = dir;
+    }
+}
