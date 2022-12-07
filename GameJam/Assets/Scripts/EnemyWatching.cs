186eaf67abd00c602c4dcc1ed835ff3298c6695e
WebDevvvv <117771890+Rustunin@users.noreply.github.com>
2022-12-07 14:53:15 +0600

    fdfd

--- GameJam/Assets/Scripts/EnemyWatching.cs
+++ GameJam/Assets/Scripts/EnemyWatching.cs
@@ -6,8 +6,12 @@ public class EnemyWatching : MonoBehaviour
 {
     public GameObject player;
     public Vector3 dir;
+    private Animator animator;
 
-
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         Vector3 targetPos = player.transform.position;
